bad56b252212bed810b5f315a06d35ac6904041a
Stefan Attoh <snoattoh@gmail.com>
2025-04-25 10:34:26 -0600

    undo array changes

--- src/utils/array.utils.ts
+++ src/utils/array.utils.ts
@@ -1,12 +1,9 @@
-import { toKebabCase } from "./string.utils";
-
 export function groupBy<T>(
   arr: T[],
   key: (item: T) => string
 ): Record<string, T[]> {
   return arr.reduce((groups, item) => {
-    const rawKey = key(item);
-    const k = toKebabCase(rawKey); // 💡 Apply camelCase here
+    const k = key(item);
     if (!groups[k]) groups[k] = [];
     groups[k].push(item);
     return groups;
