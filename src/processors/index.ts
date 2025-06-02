2072cc3e43d87ee1b4cc53b55b399cfc80ca07e3
Stefan Attoh <snoattoh@gmail.com>
2025-06-02 07:47:27 -0500

    (EGG-12) Export entire DS: Performance Optimization

--- src/processors/index.ts
+++ src/processors/index.ts
@@ -5,21 +5,24 @@ import { layoutProcessors } from './layout.processor';
 import { borderProcessors } from './border.processor';
 import { spacingProcessors } from './spacing.processor';
 
+const TEXT_PROCESSORS = fontProcessors;
+const LAYOUT_PROCESSORS = [
+  ...backgroundProcessors,
+  ...layoutProcessors,
+  ...borderProcessors,
+  ...spacingProcessors,
+];
+
 export function getProcessorsForNode(node: SceneNode): StyleProcessor[] {
   switch (node.type) {
     case 'TEXT':
-      return [...fontProcessors];
+      return TEXT_PROCESSORS;
     case 'FRAME':
     case 'RECTANGLE':
     case 'INSTANCE':
     case 'ELLIPSE':
     case 'COMPONENT':
-      return [
-        ...backgroundProcessors,
-        ...layoutProcessors,
-        ...borderProcessors,
-        ...spacingProcessors,
-      ];
+      return LAYOUT_PROCESSORS;
     default:
       return [];
   }
