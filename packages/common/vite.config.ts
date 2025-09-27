56dafdefcf94523f24a451d390f916b6ff409ba3
Mark Thompson <mthompson@bitovi.com>
2025-09-27 02:06:11 -0700

    added source maps

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -2,6 +2,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/common',
+  base: './',
   test: {
     environment: 'node',
     globals: true,
--- packages/figma/vite.config.ts
+++ packages/figma/vite.config.ts
@@ -2,35 +2,48 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-export default defineConfig(({ mode }) => ({
-  define: {
-    __DEV__: mode !== 'production',
-  },
-  build: {
-    // Avoid emptying the output since Figma will crash if the file is missing
-    // between builds.
-    emptyOutDir: false,
-    target: 'es2017', // 👈 forces esbuild to rewrite object spread, etc.
-    sourcemap: true,
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'figma',
-      fileName: () => `index.js`,
-      formats: ['iife'],
+export default defineConfig(({ mode }) => {
+  const isDev = mode !== 'production';
+  return {
+    define: {
+      __DEV__: isDev,
     },
-    rollupOptions: {
-      external: [], // ⬅ empty means "bundle everything"
-      output: {
-        // If you have any dynamic imports, inline them for a single file
-        inlineDynamicImports: true,
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/packages/figma',
+    base: './',
+    build: {
+      // Avoid emptying the output since Figma will crash if the file is missing
+      // between builds.
+      emptyOutDir: false,
+      // Forces esbuild to rewrite object spread, etc.
+      // This is needed for Figma's JS environment.
+      target: 'es2017',
+      sourcemap: isDev ? 'inline' : false,
+      minify: isDev ? false : 'esbuild',
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'figma',
+        fileName: () => `index.js`,
+        formats: ['iife'],
+      },
+      rollupOptions: {
+        external: [], // ⬅ empty means "bundle everything"
+        output: {
+          // Inline dynamic imports for a single file
+          inlineDynamicImports: true,
+        },
       },
     },
-  },
-  test: {
-    environment: 'node',
-    globals: true,
-    typecheck: {
-      tsconfig: './tsconfig.test.json',
+    esbuild: {
+      sourcemap: isDev,
+      keepNames: isDev,
     },
-  },
-}));
+    test: {
+      environment: 'node',
+      globals: true,
+      typecheck: {
+        tsconfig: './tsconfig.test.json',
+      },
+    },
+  };
+});
