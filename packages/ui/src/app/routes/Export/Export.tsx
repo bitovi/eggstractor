93df1f41ad91c9d66497bd0f4430f0abb0b595b4
Mark Thompson <mthompson@bitovi.com>
2025-10-13 20:08:30 -0500

    just to make e2e pass

--- packages/ui/src/app/routes/Export/Export.tsx
+++ packages/ui/src/app/routes/Export/Export.tsx
@@ -5,7 +5,8 @@ export const Export: FC = () => {
     <div className="container">
       <h1>Export</h1>
       <p>This is the Export page.</p>
-      <button id="generateButton">Generate Styles</button>
+      <button id="generateBtn">Generate Styles</button>
+      <p>TODO: Generated SCSS Variables</p>
     </div>
   );
 };
