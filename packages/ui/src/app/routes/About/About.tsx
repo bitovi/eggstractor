e7114c7b37ecaea619bbf8457a2a11255b47339c
Mark Thompson <mthompson@bitovi.com>
2025-10-13 20:32:10 -0500

    clean up on about

--- packages/ui/src/app/routes/About/About.tsx
+++ packages/ui/src/app/routes/About/About.tsx
@@ -1,31 +1,35 @@
 import { FC } from 'react';
+import cn from 'classnames';
 import wordmarkUrl from '../../../../assets/bitovi-wordmark.png';
 import styles from './About.module.scss';
 
 export const About: FC = () => {
   return (
-    <div className="about container">
-      <p>
+    <div className={cn('container', styles.about)}>
+      <p className={styles.p}>
         Eggstractor is an open source project for exporting design as tokens and utility layers,
         letting developers build components their way, while supporting ultra-fast, designer-led
         code updates to styling.
       </p>
-      <p>
+      <p className={styles.p}>
         By separating structure from styling, Eggstractor ensures that design changes don’t disrupt
         component architecture. And with automated code generation for updates, designers can take
         control of their styling updates, allowing for instantaneous design QA and freeing up
         developers (who now only need to review a PR for styling changes).
       </p>
-      <p>Eggstractor is an open source project by Bitovi.</p>
-      <p>
+      <p className={styles.p}>Eggstractor is an open source project by Bitovi.</p>
+      <p className={styles.p}>
         Need some help?
         <br />
-        <a>Chat with us on Discord</a> • <a>Send feature requests</a> • <a>Email us</a>
+        <a className={styles.link}>Chat with us on Discord</a> •{' '}
+        <a className={styles.link}>Send feature requests</a> •{' '}
+        <a className={styles.link}>Email us</a>
       </p>
-      <p className="last">
-        Need pros to help design & build your design system or app? <a>That's us!</a>
+      <p className={styles.last}>
+        Need pros to help design & build your design system or app?{' '}
+        <a className={styles.link}>That's us!</a>
       </p>
-      <img src={wordmarkUrl} alt="Bitovi" />
+      <img className={styles.img} src={wordmarkUrl} alt="Bitovi" />
     </div>
   );
 };
