325a0459df1d4862c343d755197a108f635bb7bb
Mark Thompson <mthompson@bitovi.com>
2025-10-13 20:05:44 -0500

    just to make e2e pass

--- packages/ui/src/app/routes/Export/Export.tsx
+++ packages/ui/src/app/routes/Export/Export.tsx
@@ -5,6 +5,7 @@ export const Export: FC = () => {
     <div className="container">
       <h1>Export</h1>
       <p>This is the Export page.</p>
+      <button id="generateButton">Generate Styles</button>
     </div>
   );
 };
